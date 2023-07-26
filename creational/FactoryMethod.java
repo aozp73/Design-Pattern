3d8ff500d3bb6ef39aae9e45e6cef509e0ae6be4
aozp73 <aozp73@naver.com>
2023-07-26 11:17:50 +0900

    생성 패턴 - 팩토리 메서드 기본구조

---
+++ creational/FactoryMethod.java
@@ -0,0 +1,48 @@
+package creational;
+
+interface Product {
+    void use();
+}
+
+class ConcreteProductA implements Product {
+    public void use() {
+        System.out.println("Using product A");
+    }
+}
+
+class ConcreteProductB implements Product {
+    public void use() {
+        System.out.println("Using product B");
+    }
+}
+
+abstract class Creator {
+    public void anOperation() {
+        Product product = factoryMethod();
+        product.use();
+    }
+    
+    protected abstract Product factoryMethod();
+}
+
+class ConcreteCreatorA extends Creator {
+    protected Product factoryMethod() {
+        return new ConcreteProductA();
+    }
+}
+
+class ConcreteCreatorB extends Creator {
+    protected Product factoryMethod() {
+        return new ConcreteProductB();
+    }
+}
+
+public class FactoryMethod {
+    public static void main(String[] args) {
+        Creator creatorA = new ConcreteCreatorA();
+        Creator creatorB = new ConcreteCreatorB();
+
+        creatorA.anOperation();
+        creatorB.anOperation();
+    }
+}
