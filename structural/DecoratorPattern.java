09dd5be55fe464ba93e9076bd5f227a1aa7ad3e6
aozp73 <aozp73@naver.com>
2023-07-26 12:17:42 +0900

    구조 패턴 - 데코레이터 기본구조

--- structural/DecoratorPattern.java
+++ structural/DecoratorPattern.java
@@ -1,7 +1,66 @@
 package structural;
 
+interface Component {
+    String operation();
+}
+
+class ConcreteComponent implements Component {
+    private String message;
+
+    public ConcreteComponent(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String operation() {
+        return message;
+    }
+}
+
+abstract class Decorator implements Component {
+    protected Component component;
+
+    public Decorator(Component component) {
+        this.component = component;
+    }
+
+    @Override
+    public String operation() {
+        return component.operation();
+    }
+}
+
+class StarDecorator extends Decorator {
+    public StarDecorator(Component component) {
+        super(component);
+    }
+
+    @Override
+    public String operation() {
+        return "*" + super.operation() + "*";
+    }
+}
+
+class UpperCaseDecorator extends Decorator {
+    public UpperCaseDecorator(Component component) {
+        super(component);
+    }
+
+    @Override
+    public String operation() {
+        return super.operation().toUpperCase();
+    }
+}
+
 public class DecoratorPattern {
     public static void main(String[] args) {
-        
+        String message = "hello, world!";
+        Component component = new ConcreteComponent(message);
+        Component starDecorator = new StarDecorator(component);
+        Component upperCaseDecorator = new UpperCaseDecorator(starDecorator);
+
+        System.out.println(component.operation());
+        System.out.println(starDecorator.operation());
+        System.out.println(upperCaseDecorator.operation());
     }
 }
